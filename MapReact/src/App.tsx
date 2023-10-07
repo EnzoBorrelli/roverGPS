6d00bcab57af535ca0bada6ef84f440d094ceda8
EnderDEV <enzoborrellidev@gmail.com>
2023-10-07 19:58:35 -0300

    added marker and coords

--- MapReact/src/App.tsx
+++ MapReact/src/App.tsx
@@ -1,20 +1,47 @@
-import React from "react";
 import "./App.css";
-import Mapa from "./components/map/Mapa";
-import Cabecera from "./components/Cabecera/Cabecera";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
+import { LngLatLike, Map, Marker } from "mapbox-gl";
 
 function App() {
+  const divMapaRef = useRef<HTMLDivElement>(null);
+  const [mapa, setMapa] = useState<Map>();
+  const [center, setCenter] = useState<LngLatLike>([-58.0001, -34]);
+  useEffect(() => {
+    mapa?.flyTo({ center });
+    if (mapa) {
+      new Marker().setLngLat(center).addTo(mapa);
+    }
+  }, [center]);
+  useEffect(() => {
+    if (divMapaRef.current) {
+      setMapa(
+        new Map({
+          container: divMapaRef.current, // container ID
+          style: "mapbox://styles/mapbox/streets-v12", // style URL
+          center, // starting position [lng, lat]
+          zoom: 9, // starting zoom
+        })
+      );
+    }
+  }, [divMapaRef]);
+
+  function Alol() {
+    setCenter([-10, -10]);
+  }
+  function Blol() {
+    setCenter([-20, -20]);
+  }
+  function Clol() {
+    setCenter([-30, -30]);
+  }
+
   return (
-    <>
-     <BrowserRouter>
-          <Cabecera />
-          <Routes>
-            <Route path="/" element={<Mapa />} />
-            <Route path="/Mapa" element={<Mapa />} />
-          </Routes>
-        </BrowserRouter>
-    </>
+    <body>
+      <button onClick={Alol}>A</button>
+      <button onClick={Blol}>B</button>
+      <button onClick={Clol}>C</button>
+      <div className="mapDiv" ref={divMapaRef}></div>
+    </body>
   );
 }
 
